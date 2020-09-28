da500a3c5ecefe828b8a7ac0f18ca78f5cde67ef
Esteban Volentini <evolentini@equiser.com.ar>
2020-09-28 13:35:52 -0300

    Version mejorada de las pruebas para aumentar la cobertura a todas las lineas de la función

--- test/suma.js
+++ test/suma.js
@@ -8,4 +8,19 @@ describe("Suma con saturacion", function() {
         expect(acumulador).to.equal(8);
         expect(error).to.equal(0);
     })
+
+    it("Maximo positivo", function() {
+        var acumulador = 32765;
+        var {acumulador, error} = suma.acumular(acumulador, 3);
+        expect(acumulador).to.equal(32767);       
+        expect(error).to.equal(1);
+    })
+
+    it("Maximo negativo", function() {
+        var acumulador = -32766;
+        var {acumulador, error} = suma.acumular(acumulador, -3);
+        expect(acumulador).to.equal(-32768);
+        expect(error).to.equal(-1);
+    })
+
 });
