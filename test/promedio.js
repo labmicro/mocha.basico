82891b94ca3d6150944b46e6f92ff1f05f5cc890
Esteban Volentini <evolentini@equiser.com.ar>
2020-11-05 09:02:55 -0300

    Otra alternativa para la implementación de la prueba promedio utilizando una función de stub

--- test/promedio.js
+++ test/promedio.js
@@ -4,9 +4,10 @@ const proxy = require("proxyquire");
 
 describe("Promedio", function() {
     it("Sin desbordamiento", function() {
-        const acumular = sinon.stub().callsFake((acumulador, operando) => {
-            return {acumulador: acumulador + operando, error: 0};
-        })
+        const acumular = sinon.stub();
+        acumular.withArgs(0, 1).returns({acumulador: 1, error: 0});
+        acumular.withArgs(1, 2).returns({acumulador: 3, error: 0});
+        acumular.withArgs(3, 3).returns({acumulador: 6, error: 0});
         const vector = [1,2,3];
 
         var promedio = proxy("../module/promedio.js", { '../module/suma.js': {'acumular': acumular }});
